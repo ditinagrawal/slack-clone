5796aee78e9dae74498a7734f7e4e8b46f4296d3
ditinagrawal <ditin.agrawal05@gmail.com>
2024-08-31 15:39:39 +0530

    Create Sidebar

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,16 +1,7 @@
-"use client";
-
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-
 const WorkSpaceId = () => {
-  const workspaceId = useWorkspaceId();
-  const { data } = useGetWorkspace({ id: workspaceId });
   return (
     <div>
-      <h1>Workspace Id</h1>
-      <p>{workspaceId}</p>
-      <p>Data: {JSON.stringify(data)}</p>
+      <h1>Workspace Id Page</h1>
     </div>
   );
 };
